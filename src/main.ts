56b4170bcba723713a9131b29122a63f6ae477ae
yosyeami <yosyeami@gmail.com>
2023-03-14 15:13:02 +0300

    swagger auth add

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,7 @@ async function bootstrap() {
     .setDescription('The Realtor API description')
     .setVersion('1.0')
     .addTag('Realtor')
+    .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
